2be3b8bf5f276e82e166f54f37f9c8a397348f19
kaqiuzi <kaqiuzi.nopi@gmail.com>
2023-09-16 23:16:41 +0800

    init 项目

---
+++ rest/src/main/java/org/miaomiao/Main.java
@@ -0,0 +1,14 @@
+package org.miaomiao;
+
+/**
+ * {@link } TODO:补充描述
+ *
+ * @author kaqiu
+ * @version 1.0, 2023/9/16
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
